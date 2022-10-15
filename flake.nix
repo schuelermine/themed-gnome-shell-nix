528101b830147c52fae63162bd59acfb06fb1d40
Anselm Schüler <mail@anselmschueler.com>
2022-10-15 23:42:46 +0200

    Fix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   outputs = { self }: {
     overlays.default = themeLoc: selfPkgs: superPkgs: {
-      gnome.gnome-shell = superPkgs.gnome.gnome-shell.overrideAttrs (selfAttrs: superAttrs: {
-        buildInputs = superAttrs.buildInputs ++ [ selfPkgs.breakpointHook ];
-        postBuild = superAttrs.postBuild or "" + ''
-          rm data/gnome-shell-theme.gresource
-          cp ${themeLoc} data/gnome-shell-theme.gresource
-        '';
-      });
+      gnome = selfPkgs // {
+        gnome-shell = superPkgs.gnome.gnome-shell.overrideAttrs (selfAttrs: superAttrs: {
+          buildInputs = superAttrs.buildInputs ++ [ selfPkgs.breakpointHook ];
+          postBuild = superAttrs.postBuild or "" + ''
+            rm data/gnome-shell-theme.gresource
+            cp ${themeLoc} data/gnome-shell-theme.gresource
+          '';
+        });
+      };
     };
   };
 }
