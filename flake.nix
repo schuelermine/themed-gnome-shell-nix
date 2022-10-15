d1b3e6504fbac5a3ad762dc095df18af2fc2b926
Anselm Schüler <mail@anselmschueler.com>
2022-10-15 23:44:22 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   outputs = { self }: {
     overlays.default = themeLoc: selfPkgs: superPkgs: {
-      gnome = superPkgs // {
+      gnome = superPkgs.gnome // {
         gnome-shell = superPkgs.gnome.gnome-shell.overrideAttrs (selfAttrs: superAttrs: {
           buildInputs = superAttrs.buildInputs ++ [ selfPkgs.breakpointHook ];
           postBuild = superAttrs.postBuild or "" + ''
