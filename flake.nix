1c31356c4c9997200b0177912d92cef839652cbc
Anselm Schüler <mail@anselmschueler.com>
2022-10-15 23:31:16 +0200

    V1

--- flake.nix
+++ flake.nix
@@ -1,18 +1,13 @@
 {
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      gresourceHandling = ./gresourceHandling.sh;
-    in
-    {
-      packages.themed-gnome-shell = theme: pkgs.gnome.gnome-shell.overrideAttrs (self: super: {
-        buildInputs = super.buildInputs ++ [ pkgs.breakpointHook ];
-        postBuild = super.postBuild or "" + ''
-          exit 1
-          PATH+=:${pkgs.glib.dev}/bin
-          workDir=$(mktemp -d)
-          source ${gresourceHandling}
+  outputs = { self }: {
+    overlays.default = themeLoc: selfPkgs: superPkgs: {
+      gnome.gnome-shell = superPkgs.gnome.gnome-shell.overrideAttrs (selfAttrs: superAttrs: {
+        buildInputs = superAttrs.buildInputs ++ [ selfPkgs.breakpointHook ];
+        postBuild = superAttrs.postBuild or "" + ''
+          rm data/gnome-shell-theme.gresource
+          cp ${themeLoc} data/gnome-shell-theme.gresource
         '';
       });
-    });
+    };
+  };
 }
