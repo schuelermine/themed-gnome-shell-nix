b78e6b8aabf33225c7c501934622bdc4480fad19
Anselm Schüler <mail@anselmschueler.com>
2022-10-16 13:52:19 +0200

    Add name change

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     overlays.default = themeLoc: selfPkgs: superPkgs: {
       gnome = superPkgs.gnome // {
         gnome-shell = superPkgs.gnome.gnome-shell.overrideAttrs (selfAttrs: superAttrs: {
+          pname = "themed-gnome-shell";
           buildInputs = superAttrs.buildInputs ++ [ selfPkgs.breakpointHook ];
           postBuild = superAttrs.postBuild or "" + ''
             rm data/gnome-shell-theme.gresource
